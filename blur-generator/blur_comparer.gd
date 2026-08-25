91d1b4a0eacd73600142f67414932624356045c5
sphynx-owner <lavisg2001@gmail.com>
2026-08-25 23:01:16 +0300

    progress

--- blur-generator/blur_comparer.gd
+++ blur-generator/blur_comparer.gd
@@ -15,13 +15,28 @@ extends Control
 		
 		return false
 
+@export var scene_record: SceneRecord
+
 @export_tool_button("load replay") var load_replay = _load_replay
 
 @export_tool_button("unload replay") var unload_replay = _unload_replay
 
 
+func _get_configuration_warnings() -> PackedStringArray:
+	var ret: PackedStringArray
+	
+	if !scene_record:
+		ret.append("scene record must be set")
+	
+	return ret
+
+
 func _load_replay() -> void:
-	await replayer.load_replay(load("res://addons/sphynx-replay-tool/temp/temp_scene_record.tres"))
+	if !scene_record:
+		push_error("no scene record is loaded to replay")
+		return
+	
+	await replayer.load_replay(scene_record)
 
 
 func _unload_replay() -> void:
